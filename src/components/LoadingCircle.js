b33a78f67d6c65b679d3d97e9576dede5c52e98b
root <root@ser3031680600.local>
2026-05-19 09:46:01 +0000

    fix: allow portfolio CRA build

--- src/components/LoadingCircle.js
+++ src/components/LoadingCircle.js
@@ -1,12 +1,7 @@
+import "./LoadingCircle.scss";
+
 const LoadingCircle = () => (
-    <div className="spinning-circle"
-    />
-  );
-  
-  // Add this CSS to your stylesheet or in a style tag
-  `@keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }`
-  
+  <div className="spinning-circle" />
+);
+
 export default LoadingCircle;
